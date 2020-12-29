498d864de8d4bf74caf34e20338facc1babf1fb0
NicolasFlandrois <n.flandrois9github@gmail.com>
2020-12-29 22:56:26 +0100

    Moved the range of days to 30 days
    This framework of canevas should be sufficiant to create "git Pixel Art".

--- git_art.py
+++ git_art.py
@@ -1,7 +1,8 @@
 import os
 
 ## Number of days you want to make commits
-for i in range(1, 365*2 + 1):
+#for i in range(1, 365*2 + 1):
+for i in range(1, 30+1):
     d = str(i) + ' day ago'
     ## Open a text file and modify it
     with open('bot.txt', 'a') as file:
