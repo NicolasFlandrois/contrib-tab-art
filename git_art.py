0a58fea82a56357090a40bb26a378743f3ccf296
NicolasFlandrois <n.flandrois9github@gmail.com>
2020-12-30 16:10:41 +0100

    Canvas work around
    How to apply Github Contrib Tab arrangements, as something usefull to apply a specific patern/canvas

--- git_art.py
+++ git_art.py
@@ -1,7 +1,9 @@
 import os
+from pprint import pprint
+import itertools
 
-## Number of days you want to make commits
-#for i in range(1, 365*2 + 1):
+# Number of days you want to make commits
+# for i in range(1, 365*2 + 1):
 for i in range(1, 30+1):
     d = str(i) + ' day ago'
     ## Open a text file and modify it
@@ -13,4 +15,86 @@ for i in range(1, 30+1):
     os.system('git commit --date="' + d + '" -m "github Art"')
 
 ## push the commit to github
-os.system('git push -u origin master')
+# os.system('git push -u origin master')
+
+
+######
+# Pprint canvas built
+# canvas_built = [["0" for i in range(7)] for n in range(25) ]
+# pprint(canvas_built)
+canvas = [
+ ['B', '0', 'x', '0', '0', '0', 'A'],
+ ['D', '0', 'x', 'x', '0', '0', 'C'],
+ ['0', '0', 'x', '0', '0', '0', '0'],
+ ['0', '0', 'x', '0', 'x', '0', '0'],
+ ['0', '0', 'x', '0', '0', 'x', '0'],
+ ['x', 'x', 'x', '0', '0', '0', '0'],
+ ['0', '0', '0', '0', '0', '0', 'x'],
+ ['0', '0', '0', '0', '0', '0', '0'],
+ ['0', '0', '0', '0', '0', '0', '0'],
+ ['x', 'x', 'x', 'x', 'x', 'x', 'x'],
+ ['0', '0', '0', 'x', '0', '0', '0'],
+ ['0', '0', '0', 'x', '0', '0', '0'],
+ ['x', 'x', 'x', 'x', 'x', 'x', 'x'],
+ ['0', '0', '0', '0', '0', '0', '0'],
+ ['0', '0', '0', '0', '0', '0', '0'],
+ ['x', 'x', 'x', 'x', 'x', '0', 'x'],
+ ['x', 'x', 'x', 'x', 'x', '0', 'x'],
+ ['0', '0', '0', '0', '0', '0', '0'],
+ ['0', '0', '0', '0', '0', '0', '0'],
+ ['x', 'x', 'x', 'x', 'x', 'x', 'x'],
+ ['0', '0', '0', 'x', '0', '0', 'x'],
+ ['0', '0', 'x', 'x', '0', '0', 'x'],
+ ['0', 'x', '0', 'x', '0', '0', 'x'],
+ ['x', '0', '0', 'x', 'x', 'x', 'x'],
+ ['0', '0', '0', '0', '0', '0', '0'],
+ ['0', '0', '0', '0', '0', '0', '0'],
+ ['x', 'x', 'x', 'x', 'x', 'x', 'x'],
+ ['x', 'x', 'x', 'x', 'x', 'x', 'x'],
+ ['x', '0', '0', 'x', '0', '0', 'x'],
+ ['x', '0', '0', 'x', '0', '0', 'x'],
+ ['x', '0', '0', '0', '0', '0', 'x'],
+ ['x', '0', '0', '0', '0', '0', 'x'],
+ ['0', '0', '0', '0', '0', '0', '0'],
+ ['G', '0', '0', '0', '0', '0', 'F']]
+pprint(canvas)
+
+# The idea is as follow:
+# If the element in the nested list is == 'x', git commit.
+# Elif the element is != 'x', do nothing.
+
+# However It shall all be in reverse.
+
+# A typical Github contribution tab canvas is as follow:
+
+#      Jan   Feb   Mar   Apr   May   Jun   Jul   Aug   Sep   Oct   Nov   Dec
+# Sun
+# Mon
+# Tue
+# Wed
+# Thu
+# Fri
+# Sat
+
+# The way my basic auto commit script goes backward Today -1, d -2, -3 etc.
+
+# Need to do:
+# Flatten my nested listings in a 1D list, and reverse it.
+# Get Index 0 of that flatten-reverse-list correspond to a specific Saturday.
+# Then decrement from there at each iteration of git commit script. (If n == 0 do nothing, else git commit "blabla".)
+
+# 1. Convert 2D Canvas into useable 1D List
+
+for n in canvas:
+    n.reverse()
+canvas_flat = list(itertools.chain(*canvas))
+print(canvas_flat)
+
+
+# 2. When is the latest Saturday?
+
+
+# 3. Start iteration through 1D canvas
+
+
+# 4. If n == "x" then commit, else do nothing.
